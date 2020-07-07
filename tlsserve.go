ebe4c4139b15b9da969418fe83a1fc6efbf9b45e
Simrut <simonruth@web.de>
2020-07-07 13:13:21 +0200

    added random secret generator

--- tlsserve.go
+++ tlsserve.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-"io"
-"log"
-"net/http"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
 )
 
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, TLS!\n")
+		//io.WriteString(w, "Hello, TLS!\n")
+		io.WriteString(w, GenerateToken(randSeq(10)))
 	})
 
 	// One can use generate_cert.go in crypto/tls to generate cert.pem and key.pem.
