e3c6fa063c0757c07534c31132668c8893c4dc22
Simrut <simonruth@web.de>
2020-07-07 15:43:08 +0200

    it works now

--- tlsserve.go
+++ tlsserve.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"io"
 	"log"
 	"math/rand"
@@ -16,6 +20,19 @@ func randSeq(n int) string {
 		b[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(b)
+}
+
+// GenerateToken returns a unique token based on the provided email string
+func GenerateToken(email string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(email), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Hash to store:", string(hash))
+
+	hasher := md5.New()
+	hasher.Write(hash)
+	return hex.EncodeToString(hasher.Sum(nil))
 }
 
 func main() {
