a66661dafeb7bb9aef589acd7ecfc724c2ab2a4d
Simrut <simonruth@web.de>
2020-07-13 13:14:11 +0200

    working handler wo/ TLS

--- tlsserve.go
+++ tlsserve.go
@@ -44,7 +44,8 @@ func main() {
 	})
 
 	// One can use generate_cert.go in crypto/tls to generate cert.pem and key.pem.
-	log.Printf("About to listen on 8443. Go to https://127.0.0.1:8443/")
-	err := http.ListenAndServeTLS(":8443", "cert.pem", "key.pem", nil)
+	log.Printf("About to listen on 8443. Go to http://127.0.0.1:8443/")
+	//err := http.ListenAndServeTLS(":8443", "cert.pem", "key.pem", nil)
+	err := http.ListenAndServe(":8443", nil)
 	log.Fatal(err)
 }
